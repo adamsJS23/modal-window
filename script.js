8e8e757a50b172fc18d8ac31b835839d2ad4e6a2
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-31 12:17:01 +0200

    Implemented modal window functionality

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 'use strict';
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+const modalWindow = document.querySelector('.modal');
+const btnCloseModal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+
+function toggleModal(){
+  modalWindow.classList.toggle('hidden');
+  overlay.classList.toggle('hidden');
+}
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', toggleModal);
+});
+
+btnCloseModal.addEventListener('click', toggleModal);
+
+document.addEventListener('keydown', function (keycode) {
+  if (keycode.keyCode === 27) {
+    toggleModal()
+  }
+});
+
+overlay.addEventListener('click', toggleModal);
